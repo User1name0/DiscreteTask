6072b7c08a1979400db64887f94a121d1633c163
User1name0 <121458125+User1name0@users.noreply.github.com>
2022-12-26 23:00:25 +0200

    Add files via upload

---
+++ Project1.cs
@@ -0,0 +1,36 @@
+﻿using System; 
+using System.Linq; 
+ 
+namespace PrimeNumbers 
+{ 
+ public static class Program 
+ { 
+  public static void Main(string[] args) 
+  { 
+   Console.WriteLine("Enter Num 1"); 
+   int Number1 = int.Parse(Console.ReadLine()); 
+    
+   Console.WriteLine("Enter Num 2"); 
+   int Number2 = int.Parse(Console.ReadLine()); 
+    
+   Console.WriteLine(" Theses are the Numbers"); 
+   for(int M = Number1 ; M <= Number2 ; M++) 
+   { 
+    int counter = 0 ; 
+    for (int A = 2 ; A<= M/2; A++ ) 
+    { 
+     if(M % A == 0) 
+    { 
+     counter++; 
+     break;
+    } 
+   } 
+      if (counter == 0 && M != 1) 
+          { 
+              Console.WriteLine("{0}", M ) ; 
+ 
+} 
+} 
+}
+}
+}
---
+++ Project2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace PerfectNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            int sum;
+
+            Console.WriteLine("Enter Num 1");
+            int Num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Num 2");
+            int Num2 = int.Parse(Console.ReadLine());
+
+            for (int x = Num1; x <= Num2; x++)
+
+            {
+
+                sum = 0;
+
+                for (int Num3 = 1; Num3 < x; Num3++)
+
+                {
+
+                    if (x % Num3 == 0)
+
+                    {
+
+                        sum += Num3;
+
+                    }
+                }
+
+                if (sum == x)
+
+                {
+
+                    Console.WriteLine(x + "  is perfect");
+
+                }
+            }
+        }
+    }
+
+}
